9857e9cdbdc73fc531f395279d95f2d8270fc7e9
Padam Chhetri <padamchhetri710@gmail.com>
2025-05-27 23:29:22 +0545

    First_day_practise

---
+++ Day 1/practise.py
@@ -0,0 +1,21 @@
+'''
+# Sum number
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("The Sum is:", a + b)
+
+#Square and print its area
+side=int(input("Enter the side of a square:"))
+print("Area=", side ** 2)
+
+#Average of Floating pioint
+num1=float(input("Enter the first number:"))
+num2=float(input("Enter the second number:"))
+print("Average=", (num1+num2)/2)
+'''
+
+#print True if a is greater then b or else print b 
+a=float(input("Enter the first number:"))
+b=float(input("Enter the second number:"))
+print(a>=b)
+
