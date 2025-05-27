8b40fe4b975cbf93956c660a28475e447b9e4b68
Padam Chhetri <padamchhetri710@gmail.com>
2025-05-27 22:59:11 +0545

    Type_Conversion

---
+++ Day 1/type_conversion.py
@@ -0,0 +1,13 @@
+#Type Conversion
+a=2
+b=1.65
+sum=a+b
+print("Type Conversion automatically sum is:",sum)
+
+#Type Casting
+a=int("2")
+b=1.65
+
+print(type(a))
+sum=a+b
+print("Type Casting Manually sum is:",sum)
