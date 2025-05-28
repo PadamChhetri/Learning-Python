026ccca3229d53add6c3ccd8cd5b46e1bbca4165
Padam Chhetri <padamchhetri710@gmail.com>
2025-05-28 22:57:09 +0545

    String_Slicing_Indexing

---
+++ Day 2/String.py
@@ -0,0 +1,21 @@
+'''
+str1="Hello"
+str2="World"
+finalstr=str1+ " " +str2
+print(finalstr) #Concatination
+
+print(len(finalstr)) #Length of String
+'''
+
+
+'''
+#Indexing
+str="Padam Chhetri"
+print(str[0])
+'''
+
+#Slicing
+str="PadamChhetri"
+print(str[ :5]) #str[0:5]
+print(str[5:]) #str[6:str(len)]
+print(str[-7:-1])
