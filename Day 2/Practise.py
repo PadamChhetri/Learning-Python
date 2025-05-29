a92ad4983b61fe143b4d8bba95b0862866a198dc
Padam Chhetri <padamchhetri710@gmail.com>
2025-05-29 22:22:48 +0545

    String_Function

---
+++ Day 2/Practise.py
@@ -0,0 +1,9 @@
+#WAP to input a user's name and print its length
+a=str(input("Enter a string:"))
+
+print("length of strging is:",len(a))
+
+
+#WAP to find the occurrence of '$' in a string
+str="Hi, Iam a $andip and i have $9.99"
+print(str.count("$"))
---
+++ Day 2/String_Function.py
@@ -0,0 +1,6 @@
+str= "my Name is Padam Chhetri"
+print(str.endswith("tri"))
+print(str.capitalize())
+print(str.replace("Padam","Mr."))
+print(str.find("P"))
+print(str.count("a"))
