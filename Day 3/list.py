384f37ae3d7dd287f55ed22d3cc92ab9f2530e88
Padam Chhetri <padamchhetri710@gmail.com>
2025-05-30 23:02:42 +0545

    List

---
+++ Day 3/list.py
@@ -0,0 +1,14 @@
+'''
+#Lists
+marks=["Padam",82,"Chhetri"]
+print(marks[1])
+print(len(marks))
+marks[0]="Mr"
+print(marks)
+'''
+
+ 
+#List Slicing
+marks=["Padam",54,"Chhetri",98,100]
+print(marks[1:4])
+print(marks[-3:-1])
---
+++ Day 3/list_method.py
@@ -0,0 +1,23 @@
+list=[2,1,3]
+list.append(4) #add one element at the end
+print(list)
+
+list.sort() #Sorts in ascending orders
+print(list)
+
+list.sort(reverse=True) #Sorts in descending orders
+print(list)
+
+list.reverse() #sorts list in reverse
+print(list)
+
+list.insert(1,5) #insert new element at 1 index
+print(list)
+
+list.remove(1) #remove the first occurance of the element
+print(list)
+
+list.pop(0) #remove element at index
+print(list)
+
+
