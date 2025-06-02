8770f8f78042b2c92e67138ad599147af7cfdcd9
Padam Chhetri <padamchhetri710@gmail.com>
2025-06-02 20:10:31 +0545

    Tuples

--- Day 3/Tuples.py
+++ Day 3/Tuples.py
@@ -1,4 +1,16 @@
-tup=(54,568,489,1654,15848)
-print(type(tup))
-print(tup[0])
-tup[0]=456  #now allowed
+tuple=(54,568,489,1654,15848,)
+# print(type(tupke))
+# print(tuple)
+# tuple[0]=456  #now allowed
+print(tuple[1:2])
+print(tuple[::2]) #means use a step of 2 — skip every other element.
+print(tuple[2:])
+print(tuple[-3:-1])
+
+# Index of its first occurrence
+index=tuple.index(54)
+print(index)
+
+# Counting the number of times a value appears in the tuple
+print(tuple.count(568))
+
