63bec2a92f6ec2ba0315d31bcc152e9a05d0367e
Padam Chhetri <padamchhetri710@gmail.com>
2025-06-03 23:25:57 +0545

    Break and continue

---
+++ Day 5/Break_Continue.py
@@ -0,0 +1,16 @@
+#Break
+i=0
+while i<=10:
+  print(i)
+  if(i==5):
+   break #terminate
+  i+=1
+  
+  #Continue
+  i=0
+  while i <= 10:
+    if(i%2==0):
+        i+=1
+        continue #skip
+    print(i)
+    i+=1
