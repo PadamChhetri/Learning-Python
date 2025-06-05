09c75ba4b8eea029333054fdf24d16006787c1a0
Padam Chhetri <padamchhetri710@gmail.com>
2025-06-05 19:50:50 +0545

    Function

---
+++ Day 6/Function.py
@@ -0,0 +1,27 @@
+
+# #Function
+# def sum(a,b): #parameters
+#   s=a+b
+#   print(s)
+#   return(s)
+# sum(5,10) #Function call; Arguments
+
+# #Average of 3 number
+# def cal_avg(a,b,c):
+#   avg=(a+b+c)/3
+#   print(avg)
+#   return (avg)
+# cal_avg(1,2,3)
+
+#Built In Fucntion
+'''
+print("College","Orchid") #sep= " "
+print("Orchid") #end= "\n"
+'''
+
+#User defined Function
+def cal_mul(a,b):
+  mul=a*b
+  print(mul)
+  return mul
+cal_mul(4,5)
---
+++ Day 6/Function_practise.py
@@ -0,0 +1,9 @@
+#WAF to find the length of the list 
+num=[8,1,2,3,23,15]
+cities=["london","Kathmandu"]
+
+def print_len(list):
+  print(len(list))
+print_len(num)
+print_len(cities)
+
