909d2f45c11cdde5f67bbb3dc7b39f2fdee37185
Padam Chhetri <padamchhetri710@gmail.com>
2025-06-07 15:05:16 +0545

    File_input_output

---
+++ Day 7/File_Input_Output.py
@@ -0,0 +1,38 @@
+'''
+#Read to a file
+f = open("demo1.txt", "r") #Open for reading
+data = f.read()
+# line=f.readline() #Read one line at a time
+print(data)
+f.close()  
+'''
+
+#Write to a file
+# f=open("demo.txt","w")
+# f.write("Hello")
+# f.close()
+
+#OR
+# f=open("demo.txt","a") #add txt in a existing or if not create a file
+# f.write("\nwelcome")
+# f.close()
+
+'''
+f=open("demo1.txt","r+")
+f.write("abc") #overwirte abc from starting of file
+data=f.read()
+print(data)
+'''
+
+'''
+f=open("demo1.txt","w+")
+f.write("abc") #trauncate(delete) all and write abc
+data=f.read()
+print(data)
+'''
+
+
+f=open("demo1.txt","a+")
+f.write("abc") #append new abc at the end
+data=f.read()
+print(data)
