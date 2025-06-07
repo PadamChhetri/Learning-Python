ff39ec3ffab3dc614b6e16bea0e236d2aa8b24f3
Padam Chhetri <padamchhetri710@gmail.com>
2025-06-07 15:11:39 +0545

    With_File

--- Day 7/with_file.py
+++ Day 7/with_file.py
@@ -2,3 +2,7 @@ with open("demo.txt","r") as f:
   data=f.read()
   print(data)
   f.close() # Not necessary in with syntax
+
+with open("demo.txt","w") as f:
+  f.write("Orchid international college")
+  
