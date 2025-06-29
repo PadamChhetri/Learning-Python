4f07d4720f1aa6a6a7e3e8a62bae8ee051f94247
Padam Chhetri <padamchhetri710@gmail.com>
2025-06-29 23:57:25 +0545

    Practise

--- intern.py
+++ intern.py
@@ -30,3 +30,95 @@ else:
 
 
     
+# class Animal:
+#   def speak(self):
+#     print("some sound")
+
+# class Dog(Animal):
+#   def speak(self):
+#       print("bark")
+# dog=Dog()
+# print(dog.speak())
+
+class car:
+  def start(self):
+    print("car started")
+  
+  def stop(self):
+    print("car stoped")
+
+class Tyotacar(car):
+  def __init__(self,name):
+    self.name=name
+    
+car1=Tyotacar("BMW")
+car2=Tyotacar("lAMBO")
+print(car1.start())
+print(car2.name)
+
+  
+'''
+#Single Inheritance
+
+class car: #parent class
+  @staticmethod
+  def start():
+    print("car strated")
+
+  @staticmethod
+  def stop():
+    print("Stop")
+
+class Tyotacar(car): #Child Class
+  def __init__(self,name):
+    self.name=name
+
+car1=Tyotacar("BMW")
+car2=Tyotacar("lAMBO")
+print(car1.start())
+print(car2.name)
+'''
+
+'''
+#Multi-Level Inheritance
+
+class car: #parent class
+  @staticmethod
+  def start():
+    print("car strated")
+
+  @staticmethod
+  def stop():
+    print("Stop")
+
+class Toyotacar(car): #Child Class
+  def __init__(self,brand):
+    self.name=brand
+
+class fortuner(Toyotacar): #Child Class
+  def __init__(self,type):
+    self.type=type
+
+car1=fortuner("diseal")
+car1.start()
+print(car1.type)
+'''
+
+'''
+#Multiple inheritance
+class A:
+  VarA="Welcome to class A"
+
+class B:
+  VarB="Welcome to class B"
+
+class C(A,B):
+  VarC="Welcome to class C"
+
+C1=C()
+
+print(C1.VarA)
+print(C1.VarB)
+print(C1.VarC)
+'''
+
